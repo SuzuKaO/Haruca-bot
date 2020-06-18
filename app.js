b434b5cf49cc0ce0f06915c608e9d69ccc421fb6
Haruca <32011143+SuzuKaO@users.noreply.github.com>
2020-06-19 00:27:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -26,22 +26,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-//TELEGRAM
 
-const TelegramBot = require('node-telegram-bot-api');
-
-//https://api.telegram.org/bot1228851710:AAFWDS14GSttAM6s3VF_uiBFFKV0TMgltJg/getUpdates?timeout=60
-
-const token = '1228851710:AAFWDS14GSttAM6s3VF_uiBFFKV0TMgltJg';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-bot.onText(/\/start/,msg =>{
-    bot.sendMessage(msg.chat.id,'AHA');
-});
-
-//END
 
 // error handler
 app.use(function(err, req, res, next) {
